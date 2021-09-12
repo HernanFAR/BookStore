df89e5d7ee3c4fc59de3d27de8e7c29c78d05f6d
Hernan Alvarez <halvarez@Austranet.com>
2021-09-12 17:02:07 -0300

    Se agrego la WebAPI de Author controller

---
+++ AuthorWebApi/Installers/InfraestructureDependencyInstaller.cs
@@ -0,0 +1,24 @@
+﻿using AuthorInfraestructure.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SharedKernel.WebAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorWebApi.Installers
+{
+    public class InfraestructureDependencyInstaller : IDependencyInstaller
+    {
+        public void InstallDependencies(IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection.AddDbContextPool<ApplicationDbContext, ApplicationDbContext>(
+                (factory, options) =>
+                {
+                    options.UseSqlServer(configuration.GetConnectionString("DbConnectionString"))
+                        .AddInterceptors(new EventInvokerInterceptor(factory.GetRequiredService<IMediator>()));
+                });
+        }
+    }
+}
