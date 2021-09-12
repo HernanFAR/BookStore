884fe011f0b55c33dcc774568b5cfbb96903d928
Hernan Alvarez <halvarez@Austranet.com>
2021-09-12 17:33:00 -0300

    Se movio el controlar a su carpeta de versionado

---
+++ AuthorWebApi/Controllers/V1/AuthorController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthorWebApi.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+    }
+}
