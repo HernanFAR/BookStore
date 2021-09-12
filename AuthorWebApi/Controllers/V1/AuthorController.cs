b072b0c65defbff8a095959a4e8b85bd287b906e
Hernan Alvarez <halvarez@Austranet.com>
2021-09-12 18:39:45 -0300

    Se agrego el controlador inicial de Author
    - Avance de feature 2.2

--- AuthorWebApi/Controllers/V1/AuthorController.cs
+++ AuthorWebApi/Controllers/V1/AuthorController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace AuthorWebApi.Controllers.V1
 {
     [Route("api/[controller]")]
     [ApiController]
+    [ApiVersion("1.0")]
+    [SwaggerTag("Crea, borra, actualiza y lee autores del sistema.")]
     public class AuthorController : ControllerBase
     {
     }
